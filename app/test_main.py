0d8b07b0e6bbc80439c7c45e261cf1e789a1d51b
Ilya Shchepetkin <90569592+RechnoyBobr@users.noreply.github.com>
2023-12-19 19:59:27 +0300

    added a test

---
+++ app/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from .main import app
+
+client = TestClient(app)
+
+def test_get():
+    res = client.get('/')
+    assert res.status_code == 200
+    assert res.json == {"message": "Hello, world!"}
